acb7b8d27e3f4684eeda7078650c6de699cf5072
dreyWesson <dreyWesson@gmail.com>
2020-06-20 13:22:19 +0100

    built chatbot with rivescript

---
+++ script.js
@@ -0,0 +1,48 @@
+let bot = new RiveScript();
+
+const message_container = document.querySelector(".messages");
+const form = document.querySelector("form");
+const input_box = document.querySelector("input");
+
+window.addEventListener("load", (event) => {
+  input_box.focus();
+});
+
+const brains = ["./brain.rive"];
+
+(async function () {
+  await bot.loadFile(brains).then(botReady).catch(botNotReady);
+})();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  selfReply(input_box.value);
+  input_box.value = "";
+});
+
+async function selfReply(message) {
+  message_container.innerHTML += `<div class="self">${message}</div>`;
+  location.href = "#edge";
+
+  await bot.reply("local-user", message).then((reply) => {
+    botReply(reply);
+    // form.scrollIntoView({
+    //   behaviour: "smooth",
+    // });
+  });
+}
+
+function botReply(message) {
+  message_container.innerHTML += `<div class="bot">${message}</div>`;
+  location.href = "#edge";
+  input_box.focus();
+}
+
+async function botReady() {
+  await bot.sortReplies();
+  botReply("Hello");
+}
+
+function botNotReady(err) {
+  console.log("An error has occurred.", err);
+}
