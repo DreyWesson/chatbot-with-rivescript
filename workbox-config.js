c9865298d7ec0484936466ccac7dfb609c5f936e
dreyWesson <dreyWesson@gmail.com>
2020-06-25 20:27:54 +0100

    built chatbot with rivescript

--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "**/*.{rive,css,html,json,js,code-workspace,mp3}"
-  ],
-  "swDest": "public/src-sw.js",
-  "swSrc": "public/sw.js"
-};
